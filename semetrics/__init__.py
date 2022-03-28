84e07768eddf115f9c9298ae913d86a5c14ff542
Hendrik Schroeter <h.schroeter@pm.me>
2022-03-28 21:58:28 +0200

    Enable multiprocessing by spawning a new cli for each call

--- semetrics/__init__.py
+++ semetrics/__init__.py
@@ -1,11 +1,12 @@
-import os
 import logging
-import oct2py
+import os
+from tempfile import TemporaryDirectory
+
+from oct2py import Oct2Py
 from pesq import pesq
 from scipy.io import wavfile
 
 logging.basicConfig(level=logging.ERROR)
-oc = oct2py.Oct2Py(logger=logging.getLogger())
 
 COMPOSITE = os.path.join(os.path.abspath(os.path.dirname(__file__)), "composite.m")
 
@@ -20,7 +21,10 @@ def pesq_mos(clean: str, enhanced: str):
 
 def composite(clean: str, enhanced: str):
     pesq_score = pesq_mos(clean, enhanced)
-    csig, cbak, covl, ssnr = oc.feval(COMPOSITE, clean, enhanced, nout=4)
+    temp_dir = TemporaryDirectory(prefix="octmp-")
+    with Oct2Py(logger=logging.getLogger(), temp_dir=temp_dir.name) as oc:
+        csig, cbak, covl, ssnr = oc.feval(COMPOSITE, clean, enhanced, nout=4)
+    temp_dir.cleanup()
     csig += 0.603 * pesq_score
     cbak += 0.478 * pesq_score
     covl += 0.805 * pesq_score
